b52119901d394eb20d20e8c69fcc753d6a771027
Aurora del Rio <auroradelrio@gmail.com>
2020-08-07 10:57:31 +0100

    others

--- Algorithms/ArraySort/QuickSort.java
+++ Algorithms/ArraySort/QuickSort.java
@@ -5,7 +5,7 @@ public class QuickSort {
     //O(n^2) THis is the worst case.
     //Quicksort uses less space than MergeSort because it sorts the array in place without a temporary arrays
     public static void main(String[] args) {
-        int[] arr = {15,9,5,10,2,11,4,7,21,6,2,12};
+        int[] arr = {9,5,10,2,11,4,7};
         int end = arr.length-1;
         arr = quickSort(arr, 0, end);
         for(int e: arr){
@@ -22,8 +22,8 @@ public class QuickSort {
     }
     public static int partition(int[] arr, int start, int end){
         int pivot = arr[end];
-        int i = -1;
-       for(int j=0; j<arr.length-1; j++){
+        int i = start-1;
+       for(int j=start; j<arr.length-1; j++){
            if(arr[j]<pivot){
                i++;
                int temp = arr[i];
@@ -32,9 +32,8 @@ public class QuickSort {
            }
        }
        i++;
-        int temp = arr[i];
-        arr[i] = arr[end];
-        arr[end]=temp;
+        arr[end]=arr[i];
+        arr[i] = pivot;
        return i;
     }
 }
